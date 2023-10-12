76c5c3db5ba2fa9ba1c3800081b41a16bbbe2754
smith <gomes.bruno201@gmail.com>
2023-10-12 02:04:09 -0300

    chore: add rule to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,12 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "import/no-default-export": "error",
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,4 @@ const App = () => {
   return <div className="font-semibold text-blue-900 sm:text-3xl">App</div>;
 };
 
-export default App;
+export { App };
