0e3383573159bf670f8015225c75f2710e2e5de8
smith <gomes.bruno201@gmail.com>
2023-10-13 21:56:37 -0300

    chore: set up colors on tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 // eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          ...colors.violet,
+          DEFAULT: colors.violet[500],
+        },
+        secondary: {
+          ...colors.gray,
+          DEFAULT: colors.gray[500],
+        },
+      },
+    },
   },
   plugins: [],
 };
